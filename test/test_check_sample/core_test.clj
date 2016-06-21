2750f65138fcd9013f21bc8ddc093ab6db227dfd
Daehyun Kim <hatemogi@gmail.com>
2016-06-21 15:22:25 +0900

    test.check 예제 적용

--- test/test_check_sample/core_test.clj
+++ test/test_check_sample/core_test.clj
@@ -1,7 +1,15 @@
 (ns test-check-sample.core-test
   (:require [clojure.test :refer :all]
-            [test-check-sample.core :refer :all]))
+            [test-check-sample.core :refer :all]
+            [clojure.test.check :as tc]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :refer [defspec]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def sort-idempotent-prop
+  (prop/for-all [v (gen/vector gen/int)]
+                (= (sort v) (sort (sort v)))))
+
+(defspec core-test
+  100
+  sort-idempotent-prop)
