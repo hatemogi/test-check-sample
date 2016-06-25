5e5bcfe3e56afd3adc447a601cd41ca11ebb288a
Daehyun Kim <hatemogi@gmail.com>
2016-06-25 12:40:55 +0900

    기본 테스트 예제 정돈

--- test/test_check_sample/basic_test.clj
+++ test/test_check_sample/basic_test.clj
@@ -1,10 +1,11 @@
-(ns test-check-sample.core-test
+(ns test-check-sample.basic-test
   (:require [clojure.test.check :as tc]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]
             [clojure.test.check.clojure-test :refer [defspec]]))
 
 (defspec 정렬-멱등성-테스트 100
+  ;; 멱등성: 연산을 여러 번 적용하더라도 결과가 달라지지 않는 성질
   (prop/for-all [v (gen/vector gen/int)]
                 (= (sort v) (sort (sort v)))))
 
@@ -13,6 +14,6 @@
                 (let [s (sort v)]
                   (< (first s) (last s)))))
 
-(defspec 실패범위-쥐어짜기 100
+(defspec 실패범위-추려내기 100
   (prop/for-all [v (gen/vector gen/int)]
                 (not (some #{42} v))))
